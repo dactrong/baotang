5ad4b905d5dd2833ac3b6ab4334be344d2b83ff1
dactrong <dactrong2001@gmail.com>
2022-10-11 10:39:03 +0700

    feat commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1 className='border pt-5'>Helllo</h1>
-     
+     <h1 className='border pt-5'>Helllo every boddy</h1>
+
     </div>
   );
 }
